faa38389b945c676d8ee524109f0ed4813f58f2e
zuzeyka <stepan.tarasenko.26@gmail.com>
2024-02-15 20:41:15 +0200

    donatello

--- app/src/main/java/com/zuzeyka/test/MainActivity.java
+++ app/src/main/java/com/zuzeyka/test/MainActivity.java
@@ -1,80 +1,20 @@
 package com.zuzeyka.test;
 
-import android.app.AlertDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.net.Uri;
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.RatingBar;
+import android.widget.ImageView;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
-
-    private int entryCount = 0;
-    private Button button;
-    private RatingBar ratingBar;
-
+public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-        ratingBar = findViewById(R.id.ratingBar);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                entryCount++;
-                if (entryCount >= 10) {
-                    showFeedbackDialog();
-                    entryCount = 0;
-                }
-            }
-        });
-
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
-                if (v >= 1 && v <= 3) {
-                    showFeedbackDialog();
-                } else if (v >= 4 && v <= 5) {
-                    openPlayStore();
-                }
-            }
-        });
-    }
-
-    private void showFeedbackDialog() {
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Leave Feedback");
-        builder.setMessage("We appreciate your feedback. Would you like to leave a review?");
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/zuzeyka"));
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                startActivity(intent);
-            }
-        });
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                dialogInterface.dismiss();
-            }
-        });
-        builder.show();
-    }
-
-    private void openPlayStore() {
-        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + getPackageName()));
-        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(intent);
+        ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setImageResource(R.drawable.d);
     }
 
     public void updateTotalPrice() {
     }
 }
+
